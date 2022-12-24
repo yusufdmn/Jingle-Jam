84f58d69ecd65400b50b1e6f4df81bf002926bb4
iMert <toptasi20@itu.edu.tr>
2022-12-24 21:06:40 +0300

    Added Singleton class

---
+++ Jingle Jam/Assets/Mert/Scripts/Helpers/Singleton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T: Singleton<T>  
+{
+    public static T instance;
+
+    public bool isPersistant;
+
+    private void Awake()
+    {
+        if (isPersistant)
+        {
+            if (instance)
+            {
+                Destroy(gameObject); 
+            }
+            else
+            {
+                instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+        else
+        {
+            instance = this as T;
+        }
+    }
+}
