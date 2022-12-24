33a87e2ffa04ee0ae7c77843f2a3185183378ed5
iMert <toptasi20@itu.edu.tr>
2022-12-24 16:54:47 +0300

    created project base + added InputManager script

---
+++ Jingle Jam/Assets/Mert/Scripts/Manager/InputManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+        // Movement
+        
+        // Interact
+        if (Input.GetMouseButton(0))
+        {
+            
+        }
+    }
+}
