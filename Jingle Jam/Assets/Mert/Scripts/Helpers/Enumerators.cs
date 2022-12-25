26c7a1aa170300b2e733c2b7adb9a35a76619805
iMert <toptasi20@itu.edu.tr>
2022-12-25 11:58:00 +0300

    Added Game Manager Script

--- Jingle Jam/Assets/Mert/Scripts/Helpers/Enumerators.cs
+++ Jingle Jam/Assets/Mert/Scripts/Helpers/Enumerators.cs
@@ -1,5 +1,7 @@
 public enum GameState
 {
     Playing = 0,
-    Pause = 1
+    Pause = 1,
+    Win = 2,
+    Lose = 3
 }
--- Jingle Jam/Assets/Mert/Scripts/Manager/GameManager.cs
+++ Jingle Jam/Assets/Mert/Scripts/Manager/GameManager.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
+    public GameState currentGameState;
+    public System.Action<GameState> OnGameStateChanged;
     
+    public void ChangeGameState(GameState state)
+    {
+        if (currentGameState != state)
+        {
+            currentGameState = state;
+            OnGameStateChanged?.Invoke(currentGameState);
+        }
+    }
 }
